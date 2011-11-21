b9eb1dd1a5eed6c4fd2c717149de7d110bd38ea7
Greg Priday <greg@siteorigin.com>
2011-11-21 22:36:50 +0200

    Fixing the kind of mistakes only a GIT newb would make

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-	'mode' => 'dev',
-	'endpoint' => 'http://192.168.1.102:8888/theme',
-	'path' => 'plugins/origin'
-);
--- js/types/text.js
+++
@@ -1,5 +0,0 @@
-jQuery(function($){
-	$('fieldset input[type=text]').add('fieldset textarea').keyup(function(){
-		previewUpdate();
-	});
-});
